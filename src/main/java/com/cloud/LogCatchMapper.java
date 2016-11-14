6983702d672b0a5790b0145335b213f7e671e038
Sagar Giri <girisagar46@gmail.com>
2016-11-14 19:39:54 +0545

    log added, mapper and main class added

---
+++ src/main/java/com/cloud/LogCatchMapper.java
@@ -0,0 +1,34 @@
+package com.cloud;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by linuxsagar on 10/21/16.
+ */
+public class LogCatchMapper extends Mapper<LongWritable, Text, Text, Text>{
+
+    private String word;
+    private int lineNumber;
+    private List<Integer> wordLine;
+
+    @Override
+    protected void setup(Context context) throws IOException, InterruptedException {
+        word = "error";
+        wordLine = new ArrayList<Integer>();
+        lineNumber = 1;
+    }
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        if(value.toString().contains(word)){
+            context.write(new Text(""),value);
+        }
+    }
+}
---
+++ src/main/java/com/cloud/StartDriver.java
@@ -0,0 +1,43 @@
+package com.cloud;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Created by linuxsagar on 10/21/16.
+ */
+public class StartDriver {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+
+        String input = "input/wordLineList.csv";
+
+        Configuration configuration = new Configuration();
+        Job wordLineCatcher = Job.getInstance(configuration);
+
+        FileInputFormat.setInputPaths(wordLineCatcher, input);
+        FileOutputFormat.setOutputPath(wordLineCatcher,new Path("logOutput.csv"));
+
+        wordLineCatcher.setInputFormatClass(TextInputFormat.class);
+        wordLineCatcher.setOutputFormatClass(TextOutputFormat.class);
+
+        wordLineCatcher.setOutputKeyClass(Text.class);
+        wordLineCatcher.setOutputValueClass(Text.class);
+
+        wordLineCatcher.setMapperClass(LogCatchMapper.class);
+
+        wordLineCatcher.setJarByClass(StartDriver.class);
+        wordLineCatcher.setJobName("Hadoop Word Line Catcher");
+
+        wordLineCatcher.waitForCompletion(true);
+    }
+}
